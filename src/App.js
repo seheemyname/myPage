a0542eafa04e4a661296a2cde8615bdf27df976c
K-sehee <106799738+K-sehee@users.noreply.github.com>
2022-12-13 20:05:01 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Back1 from './img/Back1.png';
+import Back2 from './img/Back2.png';
+import Footer from './Footer';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faAnglesDown } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='main-body'>
+      <script src="https://kit.fontawesome.com/71b0a15889.js" crossorigin="anonymous"></script>
+      <div className='back-img'>
+        <img className='Back1' alt='Back1' src={Back1}/>
+      </div>
+      <div className='back-text'>
+        <p> My name is Dicer </p>
+        <p> Front-end Web Developer </p>
+        <a href='https://github.com/seheemyname'>
+          <FontAwesomeIcon icon={faGithub}/>
+        </a> <br/> <br/>
+          <FontAwesomeIcon icon={faAnglesDown}/>
+      </div>
+      <img className='Back2' alt='Back2' src={Back2}/>
+      <Footer/>
     </div>
+      
   );
 }
 
---
+++ src/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+
+
+function Footer() {
+    return(
+        <div id="wrap">
+        <footer>
+            <p>
+                <span> dicer </span> <br/>
+                <span> E-mail : diesme@naver.com </span> <br/>
+                <span> Copyright 2022. dicer. All Rights Reserved. </span>
+            </p>
+        </footer>
+        </div>
+    );
+}
+
+export default Footer;
